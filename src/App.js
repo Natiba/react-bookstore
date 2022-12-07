1544c564549386179a0443c500469c851b94cd36
Natiba <72468185+Natiba@users.noreply.github.com>
2022-12-07 14:16:32 -0300

    tourning pages book

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 //import logo from './logo.svg';
 import './App.css';
 import TurningPages from './components/TurningPages.js'
+import Footer from './components/Footer.js';
 
 function App() {
   return (
     <div className="App">
       
      < TurningPages />
+     <Footer creator="©2022 -  Created by GRUPO 8 with love ❤️" />
     </div>
   );
 }
